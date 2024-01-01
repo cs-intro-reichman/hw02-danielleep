65c09963d435d94637433c3f2addba17ffddc48d
danielleep <113351600+danielleep@users.noreply.github.com>
2024-01-01 14:53:41 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -3,6 +3,15 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+	int x = Integer.parseInt(args[0]);
+        int counter = 1;
+        while(counter<=x)
+        {
+            if(x%counter==0)
+            {
+                System.out.println(counter);
+            }
+            counter = counter+1;
+        }
 	}
 }
