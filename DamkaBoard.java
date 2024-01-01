02dad4ac0eaadaad92212287b7e735f42bdc2622
danielleep <113351600+danielleep@users.noreply.github.com>
2024-01-01 14:52:58 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,28 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+	int n = Integer.parseInt(args[0]);
+        int x = n;
+        while (x>1) 
+        {
+            for(int i=0; i<n; i++)
+            {
+            System.out.print("* ");
+            }
+            System.out.println();
+            for(int j=0; j<n; j++)
+            {
+            System.out.print(" *");
+            }
+            System.out.println();
+            x-=2;
+        }
+        if ((n%2)!=0)
+        {
+            for(int k=0; k<n; k++)
+            {
+            System.out.print("* ");
+            }
+        }
 	}
 }
